d6eec494cc9aaa4e87a9cdaec930248e8669f88f
Koji Abel <mcgoblinprogram@gmail.com>
2023-02-18 02:14:36 -0800

    Updated Code

---
+++ build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qualcomm.ftcrobotcontroller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String APP_BUILD_TIME = "2023-02-16T22:22:46.702-0800";
+}
